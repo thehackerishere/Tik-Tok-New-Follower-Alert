554bfd0455e94e24691ee82397b300a4e4d5b8ee
thehackerishere <48867982+thehackerishere@users.noreply.github.com>
2020-08-20 21:51:24 +0100

    Update main.py

--- Examples/Play a sound/main.py
+++ Examples/Play a sound/main.py
@@ -1,9 +1,37 @@
-from playsound import *
 import time
-SOUNDDURATION = 0.2
-mp3File = ("heysisters.mp3")
-playsound(mp3File)
-time.sleep(SOUNDDURATION)
+from TikTokApi import TikTokApi
+from playsound import *
 
-#Feel free to replace the mp3 and place in the main dir
-#Make sure to replace the sound duration variable for the lenght of the sound
+api = TikTokApi()
+
+USERNAME = "tiktok"
+
+
+def get_follow():
+    test = api.getUser(USERNAME)["userInfo"]["stats"]["followerCount"]
+    return int(test)
+
+def after():
+    SOUNDDURATION = 0.2
+    mp3File = ("heysisters.mp3")
+    playsound(mp3File)
+    time.sleep(SOUNDDURATION)
+    print("New Follower")
+
+
+
+followcopy = get_follow()
+print("Followcopy :", followcopy)
+
+while True:
+    get_follows = get_follow()
+    if get_follows > followcopy:
+        loop = get_follows - followcopy
+        for loops in range(loop):
+            after()
+        followcopy = get_follow()
+
+    elif get_follows < followcopy:
+        followcopy -= 1
+        print("Lost a follower")
+    time.sleep(1)
